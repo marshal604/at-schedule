6c9c9eb3ed0dc2605f4a4e1d27f7fa41c17fcfe3
Lerdor <marshal604@gmail.com>
2020-12-20 12:46:41 +0800

    style(ScheduleHeader): refine header ui

--- src/modules/schedule/components/ScheduleHeader/ScheduleHeader.tsx
+++ src/modules/schedule/components/ScheduleHeader/ScheduleHeader.tsx
@@ -3,20 +3,20 @@ import React, { FunctionComponent } from 'react';
 import moment from 'moment-timezone';
 
 import { ScheduleHeaderProps } from './ScheduleHeader.model';
-
+import style from './ScheduleHeader.module.scss';
 const ScheduleHeader: FunctionComponent<ScheduleHeaderProps> = (props) => {
   return (
-    <div>
-      <div className="date">
-        <button onClick={() => props.prev()} disabled={props.disablePrev}>
-          {'<'}
+    <div className={style.header}>
+      <div className={style['header-date']}>
+        <button className={[style['date-button'], 'at-button'].join(' ')} onClick={() => props.prev()} disabled={props.disablePrev}>
+          <i className="fas fa-chevron-left"></i>
         </button>
-        <button onClick={() => props.next()} disabled={props.disableNext}>
-          {'>'}
+        <button className={[style['date-button'], 'at-button'].join(' ')} onClick={() => props.next()} disabled={props.disableNext}>
+          <i className="fas fa-chevron-right"></i>
         </button>
-        <div>{props.date}</div>
+        <div className={style['date-title']}>{props.date}</div>
       </div>
-      <div className="time-zone">*時間以 GMT{moment.tz(moment.tz.guess()).format('Z')} 顯示</div>
+      <div className={style['header-time-zone']}>* 時間以 GMT{moment.tz(moment.tz.guess()).format('Z')} 顯示</div>
     </div>
   );
 };
