5449c2334a36c2ac145cd38b904beae237ea0f12
Lerdor <marshal604@gmail.com>
2020-12-20 20:38:43 +0800

    feat: could detect browser language to display i18n

--- src/helper/i18n/i18n.ts
+++ src/helper/i18n/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import i18nextBrowserLangDetector from 'i18next-browser-languagedetector';
 
 import zhTw from 'src/assets/i18n/zh-TW.json';
 import en from 'src/assets/i18n/en.json';
@@ -14,11 +15,17 @@ const resources = {
   }
 };
 
+const DETECTION_OPTIONS = {
+  order: ['localStorage', 'navigator'],
+  caches: ['localStorage']
+};
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(i18nextBrowserLangDetector)
   .init({
     resources,
-    lng: 'zh-TW',
+    detection: DETECTION_OPTIONS,
     fallbackLng: 'zh-TW',
     interpolation: {
       escapeValue: false // react already safes from xss
