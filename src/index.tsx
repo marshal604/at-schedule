a0cea45b04bbd947a6e4b070aa06e782c618941d
Lerdor <marshal604@gmail.com>
2020-12-20 12:11:26 +0800

    style: add fontawesome and init style structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'src/helper/i18n/i18n';
-
+import './index.scss';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {' '}
+    <App />{' '}
   </React.StrictMode>,
   document.getElementById('root')
 );
